7246c6ecb0ced503842cdc5428e88b3df74a41dd
Twistedben <bensgamingmail@yahoo.com>
2020-06-05 19:12:11 -0400

    Initial commit

---
+++ db/migrate/20200605225215_todos.rb
@@ -0,0 +1,10 @@
+class Todos < ActiveRecord::Migration[6.0]
+  def change
+    create_table :todos do |t|
+      t.string :description
+      t.datetime :completed_at
+
+      t.timestamps
+    end 
+  end
+end
