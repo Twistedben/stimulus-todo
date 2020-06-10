234737ad6328ac9e1fae6d82a232a4c08ff997d0
Twistedben <bensgamingmail@yahoo.com>
2020-06-10 17:07:10 -0400

    Finished slideshow example

--- app/javascript/controllers/slideshow_controller.js
+++ app/javascript/controllers/slideshow_controller.js
@@ -4,24 +4,53 @@ export default class extends Controller {
   static targets = [ "slide" ]
 
   initialize() {
-    const index = parseInt(this.data.get("index")) // Essentially grabs the value from the data attribute on the controller using the name (slideshow) and then the data value
-    this.showSlide(index)
+    this.showCurrentSlide()
+    // const index = parseInt(this.data.get("index")) // Essentially grabs the value from the data attribute on the controller using the name (slideshow) and then the data value
+    // this.showSlide(index)
   }
-
+  
   next() {
-    this.showSlide(this.index + 1)  
+    this.index++
+    // this.showSlide(this.index + 1)  
   }
 
   previous() {
-    this.showSlide(this.index - 1)
+    this.index--
+    // this.showSlide(this.index - 1)
   }
 
-  showSlide(index) {
-    this.index = index
-    console.log(this.slideTargets)
+  showCurrentSlide() {
     this.slideTargets.forEach((el, i) => {
-      el.classList.toggle("slide--current", index == i)
-      el.classList.toggle('animate__slideInRight', index == i)
+      el.classList.toggle("slide--current", this.index == i)
+      el.classList.toggle('animate__slideInRight', this.index == i)
     })
   }
+
+  get index() {
+    const slidesIndexValue = this.slideTargets.length - 1 
+    if (this.data.get("index") > slidesIndexValue ) {
+      return 0
+    }
+    else if (this.data.get("index") < 0) {
+      return slidesIndexValue
+    } 
+    else {
+      return parseInt(this.data.get("index") || 0)
+    }
+  }
+
+  set index(value) {
+    this.data.set("index", value)
+    this.showCurrentSlide()
+  }
+
+  // showSlide(index) {
+  //   this.index = index
+  //   this.data.set("index", this.index)
+  //   console.log(this.slideTargets)
+  //   this.slideTargets.forEach((el, i) => {
+  //     el.classList.toggle("slide--current", index == i)
+  //     el.classList.toggle('animate__slideInRight', index == i)
+  //   })
+  // }
 }
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :people
   root "todos#index"
   resources :todos
-
+  
+  get "/messages.html", to: "messages#index"
+  
 end
